99d052af26315875b249c6aa571188378759c3a6
Murilo <mbalves1@outlook.com>
2021-04-27 09:01:40 -0300

    [Fisrt Commit]- creating repo and installing dependencies

---
+++ src/pages/Index.vue
@@ -0,0 +1,95 @@
+<template>
+  <q-page class="flex flex-center">
+    <div>
+      <q-input
+        v-model="search"
+        debounce="500"
+        filled
+        placeholder="Procurar cliente"
+        hint="Debouncing 500ms"
+      >
+        <template class="icon-search" v-slot:append>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+      <div class="flex--row card">
+        <div class="card--revenues">
+          Clientes
+        </div>
+        <div class="card--customers">
+          Busted
+        </div>
+      </div>
+      <q-card class="my-card">
+        <q-card-section class="my-card--main">
+        Murio
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card">
+        <q-card-section>
+        Murio
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card">
+        <q-card-section>
+        Murio
+        </q-card-section>
+      </q-card>
+    </div>
+    <div>
+      <q-card class="my-card">
+        <q-card-section>
+        Murio
+        </q-card-section>
+      </q-card>
+      <q-card class="my-card">
+        <q-card-section>
+        Murio
+        </q-card-section>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+};
+</script>
+<style lang="scss" scoped>
+.my-card {
+  &--main {
+    width: 500px;
+    height: 500px;
+
+    @media screen and (max-width:1280px){
+      width: 100%;
+    }
+  }
+}
+.icon-search {
+  display:flex;
+  flex-direction: end;
+}
+.card {
+  display: flex;
+  text-align: center;
+
+  @media screen and (max-width:1280px){
+    width: 100%;
+  }
+
+  &--revenues,
+  &--customers {
+    padding: 40px;
+    margin: 10px 0px;
+    width: 100%;
+    // border:1px solid black;
+    box-shadow: 0 1px 5px rgb(0 0 0 / 20%), 0 2px 2px rgb(0 0 0 / 14%);
+    border-radius: 4px;
+    vertical-align: top;
+    background: #fff;
+    position: relative;
+  }
+}
+</style>
